c5b55f85f7ead08adf345120d2b66ded509c616a
woshisimox <account4shopping@gmail.com>
2025-08-31 19:13:31 +0800

    3.7.10

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,1 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import type { AppProps } from 'next/app'; import '@/styles.css'; export default function App({Component,pageProps}:AppProps){ return <Component {...pageProps}/> }
